032bec446cafe97fe4689d9f44b1b05f4046f2ca
Cristian Ceballos <cmceballos88@gmail.com>
2023-04-08 23:37:22 +0200

    Generate main page with all the anchors and saving the data in local storage for showing in the details page

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js";
-
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
+import { generateMainTemplate, generateDetailTemplate } from './template.ts';
+const mainHtml = generateMainTemplate();
+const detailHtml = generateDetailTemplate();
+await writeFile('index.html', mainHtml);
+await writeFile('detail.html', detailHtml);
 
--- template.ts
+++ template.ts
@@ -3,11 +3,11 @@ const temtemData = await fetchData();
 const generateAnchorList = () => {
     let html = "";
     for (const temtem of temtemData) {
-        html += `<a href="detail.html">${temtem.name}</a>`;
+        html += `<a href="detail.html" data-temtem='${JSON.stringify(temtem).replace(/'/g, '')}'>${temtem.name}</a>`;
     }
     return html;
 }
-const generateMainTemplate = () => {
+export const generateMainTemplate = () => {
     const html = `
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +15,37 @@ const generateMainTemplate = () => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TemTem List</title>
+    <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    body h1 {
+        text-align: center;
+    }
+    .temtem-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-gap: 1rem;
+        margin-left: 1rem;
+        padding-top:1rem;
+
+    }
+    .temtem-list a {
+        color: #000;
+        text-decoration: none;
+        font-size: 18pt;
+        transition: all 0.5s ease;
+    }
+    .temtem-list a:hover {
+        color: lightcoral;
+        transform: scale(1.1);
+    }
+    .temtem-list a::before {
+        content: "• ";
+    }
+  </style>
 </head>
 <body>
     <h1>TemTem List</h1>
@@ -23,11 +53,23 @@ const generateMainTemplate = () => {
     ${generateAnchorList()}
     </div>
 </body>
+<script>
+function init() {
+  const temtemList = document.querySelector(".temtem-list");
+  temtemList.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      const temtemData = JSON.parse(event.target.dataset.temtem);
+      localStorage.setItem("selectedTemtem", JSON.stringify(temtemData));
+    }
+  });
+}
+window.addEventListener("load", init);
+</script>
 </html>
 `;
     return html;
 }
-const generateDetailTemplate = () => {
+export const generateDetailTemplate = () => {
     const html = `
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +80,7 @@ const generateDetailTemplate = () => {
     <title>Document</title>
 </head>
 <body>
-<div class="temtem></div>
+<div class="temtem"></div>
 
 </body>
 </html>`
